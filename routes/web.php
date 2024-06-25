896603b39e3f15613781faf7e36aa9c846fd275e
UserSCP <sebastian.pardo.scp@gmail.com>
2024-06-25 15:21:21 -0300

    Arrglo error en las dependencias(composer.json)

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
-use App\Http\Controllers\categoryController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\productsController;
-use App\Http\Controllers\brandController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\BrandController;
 
 Route::get('/', function () {return view('welcome');})->name('home');
 Route::resource('products', ProductsController::class);
-Route::resource('categories', categoryController::class);
+Route::resource('categories', CategoryController::class);
 Route::resource('brands', BrandController::class);
 Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
 Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
