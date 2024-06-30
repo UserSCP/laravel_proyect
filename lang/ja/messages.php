9fee3805204ea850ac232d3f268d95ab4e8c0c4e
UserSCP <sebastian.pardo.scp@gmail.com>
2024-06-30 13:47:21 -0300

    Subo las traducciones correctamente

---
+++ lang/ja/messages.php
@@ -0,0 +1,31 @@
+<?php
+return [
+    
+    'brand' => [
+        'created' => 'マルカ・クレアダ・コン・エキシト。',
+        'updated' => '実際の状況を確認します。',
+        'deleted' => 'マルカエリミナダコンエキシト。',
+        'load_error' => 'エラーが発生しました: :error',
+        'create_error' => 'マルカでエラーが発生しました: :error',
+        'update_error' => '実際のマルカでのエラー: :error',
+        'delete_error' => 'マルカでのエラー削除: :error',
+    ],
+    'product'=>[
+        'created'=>'製品作成と終了',
+        'updated'=>'実際に終了する製品',
+        'deleted'=>'終了後の削除',
+        'load_error'=>'製品に関するエラー: :error',
+        'create_error'=>'製品のエラー発生: :error',
+        'update_error'=>'製品の実際のエラー: :error',
+        'delete_error'=>'製品上のエラー削除: :error',
+    ],
+    'category' => [
+        'created' => 'カテゴリを作成する。',
+        'updated' => '実際のカテゴリ。',
+        'deleted' => 'カテゴリを削除する必要があります。',
+        'load_error' => 'カテゴリごとのエラー: :error',
+        'create_error' => 'カテゴリ別エラー: :error',
+        'update_error' => 'カテゴリ内の実際のエラー: :error',
+        'delete_error' => 'カテゴリ内のエラーを削除: :error',
+    ],
+];
