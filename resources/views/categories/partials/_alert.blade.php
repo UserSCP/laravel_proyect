d51b76f6f57ea4ca1b4517797d73b1c2dd78bb74
sebastian7688 <sebastianpardo583@gmail.com>
2024-07-11 15:30:41 -0300

    lol

---
+++ resources/views/categories/partials/_alert.blade.php
@@ -0,0 +1,19 @@
+<br>
+@php
+    $alerts = [
+        'delete' => 'alert1',
+        'edit' => 'alert3',
+        'create' => 'alert2',
+        'error' => 'alert6',
+    ];
+@endphp
+@foreach ($alerts as $key => $alertClass)
+    @if (session($key))
+        <div class="container">
+            <div class="{{ $alertClass }}">
+                <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+                {{ session($key) }}
+            </div>
+        </div>
+    @endif
+@endforeach
